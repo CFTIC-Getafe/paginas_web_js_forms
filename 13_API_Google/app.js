6c046b9237370ab829b6af1bc6ec4b7c96200e81
alce65 <alce65@hotmail.es>
2018-09-18 10:31:07 +0200

    Refactorización acceso al API Google

--- 13_API_Google/app.js
+++ 13_API_Google/app.js
@@ -18,25 +18,13 @@ export class App {
             url = this.URLBase + this.clave
             fetch(url, {method: 'GET'}).then(
                 response => response.json()
-            ).then(data =>  this.mostrar(data))
+            ).then(data =>  this.render(data))
         }
     }
 
-    mostrar(data) {
-        let aLibros
+    render(data) {
+        let aLibros = this.prepararDatos(data)
         let html = '<dl>'
-        console.log(data.items)
-        aLibros = data.items.map(
-            item => {
-                return {
-                    autores: item.volumeInfo.authors,
-                    titulo: item.volumeInfo.title, 
-                    icono: item.volumeInfo.imageLinks ? 
-                        item.volumeInfo.imageLinks.smallThumbnail: this.libroBase
-                }
-            }
-        )
-        console.log(aLibros)
 
          aLibros.forEach(item => {
             let autor = '&nbsp;'
@@ -50,5 +38,23 @@ export class App {
         html += '</dl>'
         this.nOutput.innerHTML = html
         this.nClave.value = ''
+    }
+
+    prepararDatos(data) {
+        let aLibros
+        console.log(data)
+        console.log(data.items)
+        aLibros = data.items.map(
+            item => {
+                return {
+                    autores: item.volumeInfo.authors,
+                    titulo: item.volumeInfo.title, 
+                    icono: item.volumeInfo.imageLinks ? 
+                        item.volumeInfo.imageLinks.smallThumbnail: this.libroBase
+                }
+            }
+        )
+        console.log(aLibros)
+        return aLibros
     }
 }
